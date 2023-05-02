75841c0f37c16bc029e6e29ccbe8e9aef8f6134a
Altynbek Kanybek <altynbekkanybek28@gmail.com>
2023-05-02 18:00:38 +0600

    adding variables

---
+++ 1-day/script.js
@@ -0,0 +1,11 @@
+// JavaScript === LiveScript
+// 1997 -> ECMA
+
+
+
+// 1 - Variables - Маалыматты озундо сактап туруучу кутуча !!!
+
+// 1.1 - var  -> "It's key word";
+// 1.2 - let  -> "It's key word";
+// 1.3 - const  -> "It's key word";
+
